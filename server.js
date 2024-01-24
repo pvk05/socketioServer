de3f01b8fb935735b55dc75109dfcceca72bf9b6
pvk05 <philip.v.knudsen@gmail.com>
2024-01-24 15:31:03 +0100

    Add .gitignore, package.json, and server.js files

---
+++ server.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+const app = express();
+const server = createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: '*',
+    },
+});
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+    // Handle socket events here
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+    });
+    socket.on("message", (msg) => {
+        console.log(msg);
+        io.emit("message", msg);
+    });
+});
+
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
