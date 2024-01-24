06465d5b184b6ad2b935747e4301c7b63b31bba7
pvk05 <philip.v.knudsen@gmail.com>
2024-01-24 15:42:24 +0100

    Update start script and port in package.json and server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const io = new Server(server, {
         origin: '*',
     },
 });
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.send('Hello, World!');
